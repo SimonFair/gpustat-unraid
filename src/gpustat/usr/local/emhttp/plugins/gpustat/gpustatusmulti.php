d780d5e0bbb7ffa6b887f986d980b9a800d096a8
Peuuuur-Noel <Peuuuur-Noel@users.noreply.github.com>
2024-05-04 22:17:32 +0000

    - Use websocket (nchan) instead of ajax polling to refresh data in dashboard - All apps usage statuses are no longer sent, only active apps

--- src/gpustat/usr/local/emhttp/plugins/gpustat/gpustatusmulti.php
+++ src/gpustat/usr/local/emhttp/plugins/gpustat/gpustatusmulti.php
@@ -30,13 +30,11 @@ include 'lib/Main.php';
 include 'lib/Nvidia.php';
 include 'lib/Intel.php';
 include 'lib/AMD.php';
-include 'lib/Error.php';
 
 use gpustat\lib\AMD;
 use gpustat\lib\Main;
 use gpustat\lib\Nvidia;
 use gpustat\lib\Intel;
-use gpustat\lib\Error;
 
 if (!isset($gpustat_cfg)) {
     $gpustat_cfg = Main::getSettings();
@@ -45,50 +43,12 @@ if (!isset($gpustat_cfg)) {
 // $gpustat_inventory should be set if called from settings page code
 if (isset($gpustat_inventory) && $gpustat_inventory) {
     $gpustat_cfg['inventory'] = true;
+
     // Settings page looks for $gpustat_data specifically -- inventory all supported GPU types
     $gpustat_data = array_merge((new Nvidia($gpustat_cfg))->getInventorym(), (new Intel($gpustat_cfg))->getInventory(), (new AMD($gpustat_cfg))->getInventorym());
-    file_put_contents("/tmp/gpuinv",json_encode($gpustat_data)) ;
-} else {
 
+    // Test data
+    // $gpustat_data = array_merge($gpustat_data, json_decode('{"0000:00:02.0": {"id": "00:02.0","model": "AlderLake-S GT1","vendor": "intel","guid": "GPU-94118c02-132c-45bb-a114-df6f24902d5d"},"0000:09:00.0": {"id": "09:00.0","model": "DG2 [Arc A770]","vendor": "intel","guid": "GPU-b13f472d-a4a7-4914-a59e-9b73c4856259"},"0000:08:00.0": {"id": "08:00.0","model": "Quadro K4000","vendor": "nvidia","guid": "GPU-ef6c0299-f1bc-7b5c-5291-7cd1a012f8bd"},"0000:0c:00.0": {"id": "0c:00.0","model": "Radeon RX 6400\/6500 XT\/6500M","vendor": "amd","guid": "GPU-639cd727-f368-4fe0-aff3-947542489448"}}', true));
 
-$array=json_decode($_GET['gpus'],true) ;
-
-
-    $data = array() ;
-    foreach ($array as $gpu) {
-        $gpustat_cfg["VENDOR"] = $gpu['vendor'] ;
-        $gpustat_cfg["GPUID"] = $gpu['guid'] ;
-        $gpustat_cfg["PCIID"] = $gpu['id'] ;
-
-    switch ($gpu['vendor']) {
-        case 'amd':
-            $return=(new AMD($gpustat_cfg))->getStatistics();
-            $decode = json_decode($return,true);
-            $decode["panel"] = $gpu['panel'] ;
-            $data[$gpu["id"]] = $decode;
-            break;
-        case 'intel':
-            $return=(new Intel($gpustat_cfg))->getStatistics();
-            $decode = json_decode($return,true);
-            $decode["panel"] = $gpu['panel'] ;
-            $data[$gpu["id"]] = $decode;
-            break;
-        case 'nvidia':
-            $return = (new Nvidia($gpustat_cfg))->getStatistics() ;
-            $decode = json_decode($return,true);
-            $decode["panel"] = $gpu['panel'] ;
-            $data[$gpu["id"]] = $decode;
-            break;
-        default:
-            print_r(Error::get(Error::CONFIG_SETTINGS_NOT_VALID));
-    }
-}
-$json=json_encode($data) ;
-#Test data
-#$json='{"00:02.0":{"clock":100,"fan":50,"memclock":500,"memutil":55,"memused":55,"power":"100W","powermax":500,"rxutil":50,"txutil":60,"temp":50,"tempmax":200,"util":"40%","vendor":"Intel","name":"AlderLake-S GT1","3drender":"50%","blitter":"50%","interrupts":100,"powerutil":"10%","video":"20%","videnh":"30%","panel":1},"03:00.0":{"clock":0,"fan":"N\/A","memclock":"N\/A","memutil":"N\/A","memused":"N\/A","power":"N\/A","powermax":"N\/A","rxutil":"N\/A","txutil":"N\/A","temp":"N\/A","tempmax":"N\/A","util":"0%","vendor":"Intel","name":"DG2 [Arc A770]","3drender":"0%","blitter":"0%","interrupts":0,"powerutil":"0%","video":"0%","videnh":"0%","panel":2},"08:00.0":{"clock":"810","fan":"30%","memclock":"2808","memutil":"50%","memused":"50","power":"28W","powermax":"87","rxutil":50,"txutil":60,"temp":"41 \u00b0C","tempmax":"101 \u00b0C","util":"77%","vendor":"NVIDIA","name":"Quadro K4000","clockmax":"810","memclockmax":"2808","memtotal":"3018","encutil":"50%","decutil":"50%","pciemax":500,"perfstate":"P0","throttled":"No","thrtlrsn":"","pciegen":2,"pciegenmax":2,"pciewidth":1,"pciewidthmax":16,"sessions":0,"uuid":"GPU-ef6c0299-f1bc-7b5c-5291-7cd1a012f8bd","plexusing":true,"plexmem":0,"plexcount":0,"jellyfinusing":true,"jellyfinmem":100,"jellyfincount":2,"handbrakeusing":false,"handbrakemem":0,"handbrakecount":0,"embyusing":false,"embymem":0,"embycount":0,"tdarrusing":false,"tdarrmem":0,"tdarrcount":0,"unmanicusing":true,"unmanicmem":0,"unmaniccount":0,"dizquetvusing":false,"dizquetvmem":0,"dizquetvcount":0,"ersatztvusing":false,"ersatztvmem":0,"ersatztvcount":0,"fileflowsusing":false,"fileflowsmem":0,"fileflowscount":0,"frigateusing":false,"frigatemem":0,"frigatecount":0,"deepstackusing":false,"deepstackmem":0,"deepstackcount":0,"nsfminerusing":false,"nsfminermem":0,"nsfminercount":0,"shinobiprousing":false,"shinobipromem":0,"shinobiprocount":0,"foldinghomeusing":false,"foldinghomemem":0,"foldinghomecount":0,"appssupp":["plex","jellyfin","handbrake","emby","tdarr","unmanic","dizquetv","ersatztv","fileflows","frigate","deepstack","nsfminer","shinobipro","foldinghome"],"panel":3},"0c:00.0":{"clock":2110.5,"fan":200,"memclock":2220.1,"memutil":"21.2%","memused":47.51,"power":50,"powermax":200,"rxutil":"N\/A","txutil":67,"temp":"38 \u00b0C","tempmax":105,"util":"90%","vendor":"AMD","name":"Radeon RX 6400\/6500 XT\/6500M","event":"80%","vertex":"70%","texture":"60%","shaderexp":"50%","sequencer":"40%","shaderinter":"30%","scancon":"30%","primassem":"30%","depthblk":"30%","colorblk":"30%","gfxtrans":"44.1%","transused":11.57,"memclockutil":"9.6%","clockutil":"21.6%","tempunit":"C","fanmax":5550,"voltage":77.7,"voltageunit":"V","panel":4}}' ;
-header('Content-Type: application/json');
-header('Content-Length:' . ES . strlen($json));
-echo $json;
-file_put_contents("/tmp/gpujson","Time = ".date(DATE_RFC2822)."\n") ;
-file_put_contents("/tmp/gpujson",$json."\n",FILE_APPEND) ;
+    file_put_contents("/tmp/gpuinv",json_encode($gpustat_data));
 }
--- src/gpustat/usr/local/emhttp/plugins/gpustat/scripts/gpustatmulti.js
+++ src/gpustat/usr/local/emhttp/plugins/gpustat/scripts/gpustatmulti.js
@@ -23,47 +23,58 @@
 */
 
 
-function toggleVFIO(vfio,panel) {
+function toggleVFIO(vfio, panel) {
     if (vfio) {
-      $('.vfio_inuse'+panel).show();
-      $('.vfio_notinuse'+panel).hide();
-      $('.vfio_status'+panel).text(_("GPU not available bound to VFIO or inuse in a VM."));
+        $('.vfio_inuse', panel).show();
+        $('.vfio_notinuse', panel).hide();
+        $('.vfio_status', panel).text(_("GPU not available bound to VFIO or inuse in a VM."));
     } else {
-      $('.vfio_inuse'+panel).hide();
-      $('.vfio_notinuse'+panel).show();
+        $('.vfio_inuse', panel).hide();
+        $('.vfio_notinuse', panel).show();
     }
-  }
+}
 
-const gpustat_statusm = (input) => {
-    $.getJSON('/plugins/gpustat/gpustatusmulti.php?gpus='+JSON.stringify(input), (data2) => {
-        if (data2) {
+const parseStats = (data2) => {
+    if (data2) {
         $.each(data2, function (key2, data) {
-            panel = data["panel"] ;
+            panel = 'tbody[data-gpu-id="' + key2 + '"]';
             if (!data['vfio']) {
-                toggleVFIO(false,panel) ;
+                toggleVFIO(false, panel);
                 switch (data["vendor"]) {
                     case 'NVIDIA':
                         // Nvidia Slider Bars
-                        $('.gpu-memclockbar'+panel).removeAttr('style').css('width', data["memclock"] / data["memclockmax"] * 100 + "%");
-                        $('.gpu-gpuclockbar'+panel ).removeAttr('style').css('width', data["clock"] / data["clockmax"] * 100 + "%");
-                        $('.gpu-powerbar'+panel ).removeAttr('style').css('width', parseInt(data["power"].replace("W","") / data["powermax"] * 100) + "%");
-                        $('.gpu-rxutilbar'+panel).removeAttr('style').css('width', parseInt(data["rxutil"] / data["pciemax"] * 100) + "%");
-                        $('.gpu-txutilbar'+panel).removeAttr('style').css('width', parseInt(data["txutil"] / data["pciemax"] * 100) + "%");
+                        $('.gpu-memclockbar', panel).removeAttr('style').css('width', data["memclock"] / data["memclockmax"] * 100 + "%");
+                        $('.gpu-gpuclockbar', panel).removeAttr('style').css('width', data["clock"] / data["clockmax"] * 100 + "%");
+                        $('.gpu-powerbar', panel).removeAttr('style').css('width', parseInt(data["power"].replace("W", "") / data["powermax"] * 100) + "%");
+                        $('.gpu-rxutilbar', panel).removeAttr('style').css('width', parseInt(data["rxutil"] / data["pciemax"] * 100) + "%");
+                        $('.gpu-txutilbar', panel).removeAttr('style').css('width', parseInt(data["txutil"] / data["pciemax"] * 100) + "%");
 
                         let nvidiabars = ['util', 'memutil', 'encutil', 'decutil', 'fan'];
                         nvidiabars.forEach(function (metric) {
-                            $('.gpu-'+metric+'bar'+panel).removeAttr('style').css('width', data[metric]);
+                            $('.gpu-' + metric + 'bar', panel).removeAttr('style').css('width', data[metric]);
                         });
 
-                        if (data["appssupp"]) {
-                            data["appssupp"].forEach(function (app) {
-                                if (data[app + "using"]) {
-                                    $('.gpu-img-span-'+app+panel).css('display', "inline");
-                                    $('#gpu-'+app+panel).attr('title', "Count: " + data[app+"count"] + " Memory: " + data[app+"mem"] + "MB");
+                        if (data["active_apps"]) {
+                            const appList = [];
+                            $('.gpu-active-apps .gpu-img-span', panel).each(function () {
+                                appList.push($(this).data('name'));
+                            });
+                            const active_apps = [];
+                            data["active_apps"].forEach(function (app) {
+                                active_apps.push(app.name);
+                                const title = 'Count: ' + app.count + ' - Memory: ' + app.mem + 'MB';
+                                if (appList.includes(app.name)) {
+                                    $('.gpu-active-apps td span[data-name="' + app.name + '"] img', panel).attr('title', title);
                                 } else {
-                                    $('.gpu-img-span-'+app+panel).css('display', "none");
-                                    $('#gpu-'+app+panel).attr('title', "");
+                                    const img = $('<img class="gpu-image" src="/plugins/gpustat/images/' + app.name + '.png" title="' + title + '">');
+                                    const span = $('<span class="gpu-img-span" data-name="' + app.name + '"></span>');
+                                    span.append(img);
+                                    $('.gpu-active-apps td', panel).append(span);
                                 }
+                            });
+                            $('.gpu-active-apps td span.gpu-img-span', panel).each(function () {
+                                if (!active_apps.includes($(this).data('name')))
+                                    $(this).remove();
                             });
                         }
                         break;
@@ -71,12 +82,12 @@ const gpustat_statusm = (input) => {
                         // Intel Slider Bars
                         let intelbars = ['3drender', 'blitter', 'video', 'videnh', 'powerutil'];
                         intelbars.forEach(function (metric) {
-                            $('.gpu-'+metric+'bar'+panel).removeAttr('style').css('width', data[metric]);
+                            $('.gpu-' + metric + 'bar', panel).removeAttr('style').css('width', data[metric]);
                         });
                         break;
                     case 'AMD':
-                        $('.gpu-powerbar'+panel).removeAttr('style').css('width', parseInt(data["power"] / data["powermax"] * 100) + "%");
-                        $('.gpu-fanbar'+panel).removeAttr('style').css('width', parseInt(data["fan"] / data["fanmax"] * 100) + "%");
+                        $('.gpu-powerbar', panel).removeAttr('style').css('width', parseInt(data["power"] / data["powermax"] * 100) + "%");
+                        $('.gpu-fanbar', panel).removeAttr('style').css('width', parseInt(data["fan"] / data["fanmax"] * 100) + "%");
                         let amdbars = [
                             'util', 'event', 'vertex',
                             'texture', 'shaderexp', 'sequencer',
@@ -85,39 +96,36 @@ const gpustat_statusm = (input) => {
                             'gfxtrans', 'memclockutil', 'clockutil'
                         ];
                         amdbars.forEach(function (metric) {
-                            $('.gpu-'+metric+'bar'+panel).removeAttr('style').css('width', data[metric]);
+                            $('.gpu-' + metric + 'bar', panel).removeAttr('style').css('width', data[metric]);
                         });
                         break;
                 }
 
                 $.each(data, function (key, data) {
                     if (key == "error") {
-                        toggleVFIO(true,panel) ;
-                        var error_text = data[0]["message"] ;
-                        $('.vfio_status'+panel).text(_(error_text));
+                        toggleVFIO(true, panel);
+                        var error_text = data[0]["message"];
+                        $('.vfio_status', panel).text(_(error_text));
                     }
-                    $('.gpu-'+key+panel).html(data);
-                    })
+                    $('.gpu-' + key, panel).html(data);
+                })
 
 
             } else {
-                toggleVFIO(true,panel) ;
-                $('.gpu-name'+panel).html(data["name"]);
-                $('.gpu-vendor'+panel).html(data["vendor"]);
+                toggleVFIO(true, panel);
+                $('.gpu-name', panel).html(data["name"]);
+                $('.gpu-vendor', panel).html(data["vendor"]);
             }
             var hidden = $.cookie('hidden_content');
             if (hidden) {
                 hidden = hidden.split(';');
-                if (hidden.includes($("#tblGPUDash" + panel).attr('title').md5())) 
-                $("#tblGPUDash" + panel ).mixedView(0);
+                if (hidden.includes($("#tblGPUDash", panel).attr('title').md5()))
+                    $("#tblGPUDash", panel).mixedView(0);
             }
-   
-            })
-         }
-   
 
-    });
-};
+        })
+    }
+}
 
 
 /*
